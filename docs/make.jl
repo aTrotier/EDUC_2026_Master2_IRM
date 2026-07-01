4884cf7a73a08168784af69493a1e36481f10e64
aTrotier <a.trotier@gmail.com>
2026-07-01 09:53:23 +0200

    add deploy

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,16 @@ using DocumenterVitepress
 makedocs(;
     # ... your other arguments ...
     format = DocumenterVitepress.MarkdownVitepress(
-        repo = "github.com/CRMSB/EDUC_2026_Master2_IRM",
+        repo = "github.com/aTrotier/EDUC_2026_Master2_IRM",
         devbranch = "main", # or master, trunk, ...
         devurl = "dev",
     ),
+)
+
+DocumenterVitepress.deploydocs(;
+    repo = "github.com/aTrotier/EDUC_2026_Master2_IRM",
+    target = joinpath(@__DIR__, "build"),
+    branch = "gh-pages",
+    devbranch = "main",
+    push_preview = true,
 )
